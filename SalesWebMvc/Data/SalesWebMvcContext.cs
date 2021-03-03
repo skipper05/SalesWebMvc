6e57c51235d7c663f3608d8d084cf3d0ead2ccef
Andre Oliveira <oliveiraa80@gmail.com>
2021-03-03 07:53:18 -0300

    Seeding Service

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace SalesWebMvc.Models
 {
     public class SalesWebMvcContext : DbContext
--- SalesWebMvc/Models/Enums/SaleStatus.cs
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -9,6 +9,6 @@ namespace SalesWebMvc.Models.Enums
     {
         Pending = 0,
         Billed = 1,
-        Cancelled = 2
+        Canceled = 2
     }
 }
